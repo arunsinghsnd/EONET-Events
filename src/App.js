b881ca8c467f91270989e611c683c04caf3a941d
Arun Singh <arunsinghsnd@gmail.com>
2021-06-20 08:00:26 +0530

    render the google map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Map from "./components/Map";
 
 const App = () => {
   return (
     <div>
-      <h1>Welcome to Fire Tracker</h1>
+      <Map />
     </div>
   );
 };
